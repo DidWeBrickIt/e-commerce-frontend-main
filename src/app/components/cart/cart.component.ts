1808346e0c70c8e43642944818eb8c2d10ac2654
smattson613 <smattson613@gmail.com>
2022-09-14 15:22:17 -0400

    Added Remove from Cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
@@ -10,6 +10,7 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class CartComponent implements OnInit {
 
+  cartCount!: number;
   products: {
     product: Product,
     quantity: number
@@ -17,11 +18,14 @@ export class CartComponent implements OnInit {
   totalPrice!: number;
   cartProducts: Product[] = [];
 
+  @Input() productInfo!: Product;
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
     this.productService.getCart().subscribe(
       (cart) => {
+        this.cartCount = cart.cartCount;
         this.products = cart.products;
         this.products.forEach(
           (element) => this.cartProducts.push(element.product)
@@ -39,6 +43,30 @@ export class CartComponent implements OnInit {
     };
     this.productService.setCart(cart);
     this.router.navigate(['/home']);
+  }
+
+  removeFromCart(product: Product): void {
+    
+    let inCart = true;
+    
+    this.products.forEach(
+      (element) => {
+        if(element.product == product){
+          --element.quantity; 
+          let cart = {
+            cartCount: this.cartCount - 1,
+            products: this.products,
+            totalPrice: this.totalPrice - product.price
+          };
+          if (element.quantity === 0) {
+            cart.products = cart.products.filter(p => !(p.product == product));
+          }
+          this.productService.setCart(cart);
+          inCart=true;
+          return;
+        };
+      }
+    );  
   }
 
 }
