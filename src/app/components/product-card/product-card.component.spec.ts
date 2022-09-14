5be51ba0144590478d17a33e957a84054bd87fd1
dillonconner <dillonconner@gmail.com>
2022-09-14 17:16:34 -0400

    i forgot what changed but something did

--- src/app/components/product-card/product-card.component.spec.ts
+++ src/app/components/product-card/product-card.component.spec.ts
@@ -62,12 +62,13 @@ describe('ProductCardComponent', () => {
     const testProduct2 = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
     component.addToCart(testProduct1);
     component.addToCart(testProduct1);
+    expect(component.products[0].quantity).toBe(2);
     expect(component.products.length).toBe(1);
   });
 
   it('should add new product', () => {
     const testProduct1 = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
-    const testProduct2 = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
+    const testProduct2 = new Product(2, "dirt2", 1, "its dirt v2", 2000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
     component.addToCart(testProduct1);
     component.addToCart(testProduct2);
     expect(component.products.length).toBe(2);
