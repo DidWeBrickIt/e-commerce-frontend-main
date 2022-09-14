521d0a87ac946ccb90c917a17b40888c5c08edd6
dillonconner <dillonconner@gmail.com>
2022-09-14 14:48:12 -0400

    fixed image underfined error

--- src/app/components/product-card/product-card.component.spec.ts
+++ src/app/components/product-card/product-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
 import { ProductCardComponent } from './product-card.component';
@@ -9,11 +11,34 @@ describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
 
+  interface Cart {
+    cartCount: number;
+    products: {
+      product: Product,
+      quantity: number
+    }[];
+    totalPrice: number;
+  }
+
+  class MockProductService{
+
+    getCart(): Observable<Cart> {
+      const testProduct: Product = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
+      const testCart = new BehaviorSubject<Cart>({
+        cartCount: 1,
+        products: [{product:testProduct, quantity:1}],
+        totalPrice: 1.00
+      });
+      return testCart;
+    }
+  }
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ProductCardComponent ],
       imports: [HttpClientModule]
+      //providers: [{provide: ProductService, useClass: MockProductService}]
     })
     .compileComponents();
   });
@@ -21,10 +46,20 @@ describe('ProductCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
+    component.productInfo = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add to product quantity', () => {
+    
+  });
+
+  it('should add to product quantity', () => {
+
+  });
+
 });
