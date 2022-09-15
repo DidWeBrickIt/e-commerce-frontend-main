8ba818d462c6adf39da7b36d6d38ded7d82230f5
jalec <jaleckc@gmail.com>
2022-09-15 15:07:28 -0700

    update angular testing

--- src/app/models/profile.spec.ts
+++ src/app/models/profile.spec.ts
@@ -2,6 +2,6 @@ import { Profile } from './profile';
 
 describe('Profile', () => {
   it('should create an instance', () => {
-    expect(new Profile()).toBeTruthy();
+    expect(new Profile("first","last","test@gmail.com","password")).toBeTruthy();
   });
 });
