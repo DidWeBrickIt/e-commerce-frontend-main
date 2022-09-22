acd4bf498570a1c70ac547d2eb4d49a75ac4211b
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-09-22 09:40:32 -0700

    remove cvv

--- src/app/models/payment/payment.ts
+++ src/app/models/payment/payment.ts
@@ -1,12 +1,10 @@
 export class Payment {
 
   credit_card_number: string;
-  security_code: string;
   expiration: string;
 
-  constructor(ccn: string, cvv: string, exp: string) {
+  constructor(ccn: string, exp: string) {
     this.credit_card_number = ccn;
-    this.security_code = cvv;
     this.expiration = exp;
   }
 }
