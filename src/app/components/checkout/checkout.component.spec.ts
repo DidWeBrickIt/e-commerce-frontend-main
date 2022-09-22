6cf982f479408f4046273b3c5e4e3cb1ea6e6276
Timothy Wu <twu23@buffalo.edu>
2022-09-22 15:07:12 -0400

    added lots of testing

--- src/app/components/checkout/checkout.component.spec.ts
+++ src/app/components/checkout/checkout.component.spec.ts
@@ -4,9 +4,14 @@ import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testin
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
+import { Address } from 'src/app/models/address/address';
 import { Order } from 'src/app/models/order/order';
+import { Payment } from 'src/app/models/payment/payment';
 import { Product } from 'src/app/models/product/product';
+import { Profile } from 'src/app/models/profile/profile';
+import { User } from 'src/app/models/user/user';
 import { ProductService } from 'src/app/services/product/product.service';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 import { OrdersComponent } from '../orders/orders.component';
 
 import { CheckoutComponent } from './checkout.component';
@@ -16,12 +21,13 @@ describe('CheckoutComponent', () => {
   let fixture: ComponentFixture<CheckoutComponent>;
   let routerSpy = {navigate: jasmine.createSpy('navigate')};
   let service: ProductService;
+  let service2: ProfileService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CheckoutComponent ],
       imports: [HttpClientTestingModule, HttpClientModule],
-      providers: [ProductService, {provide: Router, useValue: routerSpy},FormBuilder]
+      providers: [ProductService, ProfileService, {provide: Router, useValue: routerSpy},FormBuilder]
     })
     .compileComponents();
   });
@@ -31,16 +37,23 @@ describe('CheckoutComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     service = TestBed.inject(ProductService);
+    service2 = TestBed.inject(ProfileService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should retrieve profile', fakeAsync(() => {
+    let userProf: Profile = new Profile(new User("test", "tester", "test@test.test", "test"), new Payment("test", "test", "test"), new Address("test", "test", "test", "test", "test", "test"));
+    let spy = spyOn(service2, 'getProfileInfo').and.returnValue(of(userProf));
+    component.ngOnInit();
+  }));
+
   it('should just navigate home', fakeAsync(() => {
     let spy = spyOn(service, 'purchase').and.returnValue(of([]));
     
-    component.onSubmit();
+    component.purchase();
     tick();
     
     expect(component.finalProducts.length).toBe(0);
@@ -49,19 +62,13 @@ describe('CheckoutComponent', () => {
   }));
 
   it('should purchase then navigate home', fakeAsync(() => {
-    const testProduct1 = new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
-    component.products = [{product:testProduct1, quantity:2}];
+    const testProduct1 = [new Product(1, "dirt", 1, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1")];
+    component.finalProducts = [{id:1, quantity:2}];
+    let spy = spyOn(service, 'purchase').and.returnValue(of(testProduct1));
 
-    let spy = spyOn(service, 'purchase').and.returnValue(of([]));
-    let subSpy = spyOn(service.purchase(component.finalProducts), 'subscribe');
-    
-    component.onSubmit();
+    component.purchase();
     tick();
-
-    expect(component.finalProducts.length).toBe(0);
-    expect(spy).toHaveBeenCalledBefore(subSpy);
-    expect(subSpy).toHaveBeenCalled();
-    
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/home']);
   }));
 
 
@@ -78,5 +85,13 @@ describe('CheckoutComponent', () => {
     tick();
 
     expect(component.orders.length).toBe(0);
+  }));
+
+  it('should make an order', fakeAsync(() => {
+    const testProduct1 = new Product(1, "dirt", 10, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
+    component.products.push({product:testProduct1, quantity:5});
+    let spy = spyOn(service, 'getUserId').and.returnValue(of([1]));
+    component.makeOrder();
+    expect(component.orders.length).toEqual(1);
   }));
 });
--- src/app/components/orders/orders.component.spec.ts
+++ src/app/components/orders/orders.component.spec.ts
@@ -1,25 +1,73 @@
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProductService } from 'src/app/services/product/product.service';
+import { of } from 'rxjs';
 
 import { OrdersComponent } from './orders.component';
+import { CheckoutComponent } from '../checkout/checkout.component';
+import { Product } from 'src/app/models/product/product';
+import { Order } from 'src/app/models/order/order';
+import { FormBuilder } from '@angular/forms';
 
 describe('OrdersComponent', () => {
   let component: OrdersComponent;
+  let component2: CheckoutComponent;
   let fixture: ComponentFixture<OrdersComponent>;
+  let fixture2: ComponentFixture<CheckoutComponent>;
+  let service: ProductService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ OrdersComponent ],
-      imports: [HttpClientModule]
+      declarations: [ OrdersComponent, CheckoutComponent ],
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [ ProductService, FormBuilder ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(OrdersComponent);
+    fixture2 = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
+    component2 = fixture2.componentInstance;
+    service = TestBed.inject(ProductService);
     fixture.detectChanges();
+    fixture2.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should format user orders', () => {
+    const testProduct1 = new Product(1, "dirt", 10, "its dirt", 1000.00, "https://i0.wp.com/christianlydemann.com/wp-content/uploads/2018/10/angular-test-one-does-not.jpg?fit=490%2C288&ssl=1");
+    component2.products.push({product:testProduct1, quantity:5});
+    let spy = spyOn(service, 'getUserId').and.returnValue(of([1]));
+    component2.makeOrder();
+    let orders: Order[] = component2.orders;
+    let product: Product = testProduct1;
+    let spy2 = spyOn(service, 'getOrdersByUserId').and.returnValue(of(orders));
+    let spy3 = spyOn(service, 'getSingleProduct').and.returnValue(of(product));
+    component.formatUserOrders();
+    expect(component.orders.length).toEqual(orders.length);
+  });
+
+  it('should consolidate orders', () => {
+    let orders: {orderId: number, productName: string, productAmount: number, cost: number, time: string}[] = [];
+    orders.push({orderId: 1, productName: "test", productAmount: 5, cost: 500, time: "100000"});
+    orders.push({orderId: 2, productName: "test2", productAmount: 50, cost: 5000, time: "100000"});
+    component.consolidateOrders(orders);
+    expect(component.consolidatedOrders.length).toEqual(1);
+  })
+
+  it('should toggle details', () => {
+    let tempBool = !component.showDetails;
+    component.toggleDetails(5);
+    expect(component.orderDetails).toEqual(5);
+    expect(component.showDetails).toEqual(tempBool);
+  });
+
+  it('should stop prop', () => {
+    let event = new Event('click');
+    component.stopProp(event);
   });
 });
