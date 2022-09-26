2eb21a6b0e6e30891f70f8038dd6885385097e12
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-09-26 08:21:41 -0700

    remove payment

--- src/app/models/payment/payment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Payment } from './payment';
-
-describe('Payment', () => {
-    it('should create payment', () => {
-        expect(new Payment("1234567812345678","07/2027")).toBeTruthy();
-    })
-});
