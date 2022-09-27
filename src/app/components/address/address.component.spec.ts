7774fb438b982f2b265ea6ef79d1f99ea82e5f5b
dillonconner <dillonconner@gmail.com>
2022-09-27 11:01:55 -0400

    fixed should create tests

--- src/app/components/address/address.component.spec.ts
+++ src/app/components/address/address.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { Address } from 'src/app/models/address/address';
 
 import { AddressComponent } from './address.component';
 
@@ -8,12 +11,15 @@ describe('AddressComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddressComponent ]
+      declarations: [ AddressComponent ],
+      imports: [HttpClientTestingModule ],
+      providers: [FormBuilder]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AddressComponent);
     component = fixture.componentInstance;
+    component.address = new Address("1234 Angular St", "Unit 56", "Portland", "OR", "12345", "USA");
     fixture.detectChanges();
   });
 
