a30342148a86158051335e47c2a62776096bf7d0
Sunny <faronwmoore@gmail.com>
2022-09-27 09:49:46 -0500

    Removed tech chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,8 +35,6 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ChatComponent } from './components/chat/chat.component';
 import { PaypalComponent } from './components/paypal/paypal.component';
 import { NgxPayPalModule } from 'ngx-paypal';
 import { CreateProductComponent } from './components/create-product/create-product.component';
@@ -66,8 +64,6 @@ import { CreateProductComponent } from './components/create-product/create-produ
     ProfilePicComponent,
     ResetPasswordComponent,
     PaypalComponent,
-    FooterComponent,
-    ChatComponent,
     CreateProductComponent,
   ],
 
